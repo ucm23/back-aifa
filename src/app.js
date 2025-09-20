79917fa11b5443f139edcef0e651d5db90e7b81f
Ulises Cano Martinez <eziocano23@hotmail.com>
2025-09-19 20:55:56 -0600

    (ADD) PARSE v2 19-09-2025

--- src/app.js
+++ src/app.js
@@ -11,9 +11,11 @@ const app = express();
 const api = '/api'
 
 app.use(morgan("dev"));
-//app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.urlencoded({ extended: true }));
 //app.use(express.static(path.join(__dirname, 'public')))
 
